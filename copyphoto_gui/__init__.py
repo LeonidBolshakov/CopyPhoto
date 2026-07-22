4bf221544d58d7537a11a10a4f795b5b35268dd9
LeonidBolshakov <bolleoa@gmail.com>
2026-07-22 22:24:34 +0800

    Добавление __init__.py

---
+++ copyphoto_gui/__init__.py
@@ -0,0 +1,13 @@
+"""Компоненты графического интерфейса CopyPhoto."""
+
+from copyphoto_gui.directory_widget import DirectoryWidget, ImagePreview
+from copyphoto_gui.main_window import APP_TITLE, MainWindow
+from copyphoto_gui.settings_widget import SettingsWidget
+
+__all__ = [
+    "APP_TITLE",
+    "DirectoryWidget",
+    "ImagePreview",
+    "MainWindow",
+    "SettingsWidget",
+]
